9c7dd46a9156d0658f0ee6140611e9cda61c6f9f
pmm-quang <63343138+pmm-quang@users.noreply.github.com>
2020-10-05 00:51:25 +0700

    Update Controller.java

--- Controller.java
+++ Controller.java
@@ -7,6 +7,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -24,9 +25,28 @@ public class Controller implements Initializable {
     @FXML
     public TextArea taMeaning;
 
+    String Word = "";
     DictionaryMangement dictionary = new DictionaryMangement();
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
+        tfSearchedWord.setOnKeyReleased(event -> {
+            String searchedWord = tfSearchedWord.getText();
+                if (event.getCode() != null && ! event.getCode().equals(KeyCode.ENTER)) {
+                    Word = searchedWord;
+                //    String searchedWord = tfSearchedWord.getText();
+                    dictionary.deleteListSame();
+                    lvWords.getItems().clear();
+                    lvWords.getItems().addAll(dictionary.Same(Word));
+
+                    System.out.println(Word);
+                } else if (event.getCode().equals(KeyCode.ENTER)) {
+                    String wordMeaning = dictionary.Interpretation(searchedWord);
+                    taMeaning.setText(wordMeaning);
+                    Word = null;
+                }
+        });
+
         btSearch.setOnMouseClicked(event -> {
             System.out.println("Search!!!");
             String searchedWord = tfSearchedWord.getText();
@@ -50,5 +70,6 @@ public class Controller implements Initializable {
     public void initializeWordList() {
         dictionary.insertFromFile();
         lvWords.getItems().addAll(dictionary.getDictionary().keySet());
+    //    lvWords.getItems().addAll(dictionary.Same("He"));
     }
 }
